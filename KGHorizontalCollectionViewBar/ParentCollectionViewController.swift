8f420b25516ac21289ec69d8099a01e04b46078b
Kevin Gödecke <goedecke@net-studios.de>
2015-11-18 18:18:31 +0100

    Initial Import

---
+++ KGHorizontalCollectionViewBar/ParentCollectionViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  CollectionContainerViewController
+//
+//  Created by Kevin Goedecke on 11/18/15.
+//  Copyright © 2015 kevingoedecke. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "CollectionCell"
+
+class ParentCollectionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
+
+extension ParentCollectionViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(collectionView: UICollectionView,
+        numberOfItemsInSection section: Int) -> Int {
+            return 10
+    }
+    
+    func collectionView(collectionView: UICollectionView,
+        cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+            
+            let cell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier,
+                forIndexPath: indexPath)
+            return cell
+    }
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+}
